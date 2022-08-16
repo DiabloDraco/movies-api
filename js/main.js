d03d999d2a7207f827cd2ff0158657f22e0a25f5
DiabloDraco <mirmuhsinmirahmatov@gmail.com>
2022-08-16 16:58:24 +0500

    add

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
-fetch("https://www.omdbapi.com/?s=avengers&apikey=3c34912c&page=2")
+let elInput = document.querySelector("#input")
+
+elInput.addEventListener("input" , function (evt) {
+    let input = elInput.value.trim()
+    fetch(`https://www.omdbapi.com/?s=${input}&apikey=3c34912c&page=2`)
     .then(res => res.json())
-    .then(data => render(data.Search))
+    .then(data => render(data))
+
+})
+
 
 function render(array) {
     let elWrapper = document.querySelector("#list")
     let fragment = document.createDocumentFragment()
-
-    for (let i = 0; i < array.length; i++) {
+    elWrapper.innerHTML = null
+    for (let i = 0; i < array.Search.length; i++) {
     let newLi = document.createElement("li")
     let newImg = document.createElement("img")
     let newh2 = document.createElement("h2")
@@ -16,9 +23,9 @@ function render(array) {
     newLi.appendChild(newh2)
     newLi.appendChild(newP)
     newLi.classList.add("d-flex" , "flex-column" , "justify-content-center", "align-items-center")
-    newImg.src = array[i].Poster
-    newh2.textContent = `Title: ${array[i].Title}`
-    newP.textContent = `Year: ${array[i].Year}`
+    newImg.src = array.Search[i].Poster
+    newh2.textContent = `Title: ${array.Search[i].Title}`
+    newP.textContent = `Year: ${array.Search[i].Year}`
     fragment.appendChild(newLi)
     }
     elWrapper.appendChild(fragment)
